8133ff16863f6c105890f95300ff686e296957ac
Tiffany Chang <tiffany@starlight.money>
2022-07-10 11:28:44 -0500

    merged in resy bot

--- app.js
+++ app.js
@@ -1,59 +1,56 @@
+require("dotenv").config();
+import { runResyJobs } from "./resy-api/src/monitor.ts"; 
+require('./path').Message
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const { App } = require("@slack/bolt");
+const fs = require('fs')
+let raw = fs.readFileSync('db.json');
+let events= JSON.parse(raw);
 
-app.get("/", (req, res) => res.type('html').send(html));
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.APP_TOKEN
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.message(/hi/, async ({ command, say }) => {
+  try {
+    say("Hi Neil 🙆‍♀️");
+    runResyJobs();
+  } catch (error) {
+      console.log("err")
+    console.error(error);
+  }
+});
 
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+
+app.command("/next", async ({ command, ack, say }) => {
+  try {
+    await ack();
+    say("Yaaay! that command works?");
+  } catch (error) {
+      console.log("err")
+    console.error(error);
+  }
+});
+
+// save data to db.json
+// fs.readFile("db.json", function (err, data) {
+//   const json = JSON.parse(data);
+//   json.data.push(newFAQ);
+//   fs.writeFile("db.json", JSON.stringify(json), function (err) {
+//     if (err) throw err;
+//     console.log("Successfully saved to db.json!");
+//   });
+// });
+
+
+
+(async () => {
+  const port = 3001
+  // Start your app
+  await app.start(process.env.PORT || port);
+  console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+})();
